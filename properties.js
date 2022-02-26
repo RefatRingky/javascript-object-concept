ea04ec5a04df305ea99255b39430bd3f17251370
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-26 14:43:08 +0600

    object commit

--- properties.js
+++ properties.js
@@ -2,15 +2,29 @@ const bottle = {
     color:'yellow' ,
     hold:'water',
     isCleaned:true,
-    price:123
+    price:123,
 };
 
 // getting key from object using key method
 const keys = Object.keys(bottle);
-console.log('The Key is:',keys);
+// console.log('The Key is:',keys);
+
+
 // getting value from object using value method
 const values = Object.values(bottle)
-console.log('The value is:',values);
+// console.log('The value is:',values);
+
+
 // gettin entries form an object using entries method
 const pairs = Object.entries(bottle);
-console.log('The Pair is',pairs);
+// console.log('The Pair is',pairs);
+
+
+// delete properties from object
+delete bottle.isCleaned;
+// console.log(bottle);
+
+// seal method
+Object.seal(bottle);
+bottle.color = 'red';
+console.log(bottle);
