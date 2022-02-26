580e028ce9d3dc67066f695d8749634fb24e8a79
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-26 14:31:09 +0600

    first commit

---
+++ properties.js
@@ -0,0 +1,16 @@
+const bottle = {
+    color:'yellow' ,
+    hold:'water',
+    isCleaned:true,
+    price:123
+};
+
+// getting key from object using key method
+const keys = Object.keys(bottle);
+console.log('The Key is:',keys);
+// getting value from object using value method
+const values = Object.values(bottle)
+console.log('The value is:',values);
+// gettin entries form an object using entries method
+const pairs = Object.entries(bottle);
+console.log('The Pair is',pairs);
